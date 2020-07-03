5fefecab722296f45b1481339511a9d6feedeb88
MRROBO28 <67741815+MRROBO28@users.noreply.github.com>
2020-07-03 15:58:43 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg install php -y
+apt install toilet -y
+apt install ruby -y
+apt install neofetch -y
+apt install cowsay -y
+gem install lolcat 
+pip install mechanize
+pip2 install mechanize
+pip install requests
+pip2 install requests
+pkg install wget -y
+
